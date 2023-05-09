437e37cadbadd11dba410befb68de36fdc58ef61
elifakalin <elif.armagan@finartz.com>
2023-05-09 13:27:02 +0300

    Initial commit

---
+++ src/main/java/com/example/account/currency/exchange/service/AccountCurrencyExchangeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.account.currency.exchange.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountCurrencyExchangeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountCurrencyExchangeServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/account/currency/exchange/service/AccountCurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.account.currency.exchange.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountCurrencyExchangeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
